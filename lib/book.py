762f1a09121d87ec1836a6f68dcf98302c8a6092
John Shoemaker <shoemjoh@gmail.com>
2024-10-10 20:18:18 -0400

    working object lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if(isinstance(page_count, int)):
+            print(f"Setting page count to {page_count}")
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
